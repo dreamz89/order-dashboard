41b60c0911a161ac061fb0171cde2fb34c600b4b
Jocelyn Tan <jocelyntan89@hotmail.com>
2021-01-23 14:18:55 +0100

    restyle toggle

--- src/shared/Toggle.jsx
+++ src/shared/Toggle.jsx
@@ -30,13 +30,23 @@ const Outer = styled.div`
 `
 
 const Inner = styled.div`
-  color: ${ props => props.active === props.choice && '#304ffe'};
+  background-color: ${ props => props.active === props.choice && '#D6F5FF'};
+  color: ${ props => props.active === props.choice ? '#304ffe' : '#666'};
   cursor: pointer;
   font-family: 'Montserrat', sans-serif;
-  font-size: 14px;
-  padding: 0 8px;
+  font-size: 12px;
+  font-weight: 400;
+  padding: 6px 12px;
 
   &:first-child {
-    border-right: 1px solid gray;
+    border: 1px solid ${ props => props.active === props.choice ? '#304ffe' : '#666'};
+    border-right: ${ props => props.active !== props.choice && 0};
+    border-radius: 4px 0 0 4px;
+  }
+
+  &:last-child {
+    border: 1px solid ${ props => props.active === props.choice ? '#304ffe' : '#666'};
+    border-left: ${ props => props.active !== props.choice && 0};
+    border-radius: 0 4px 4px 0;
   }
 `
