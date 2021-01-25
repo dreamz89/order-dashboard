c7cdca4b75e6a3a882900d54491830521939f39e
Jocelyn Tan <jocelyntan89@hotmail.com>
2021-01-25 13:24:44 +0100

    remove useEffect dependency

--- src/components/suppliers-rank/SuppliersRank.jsx
+++ src/components/suppliers-rank/SuppliersRank.jsx
@@ -37,57 +37,54 @@ function SuppliersRank({ data }) {
   const sortedData = groupedSuppliers.sort((a, b) => a[active] - b[active])
 
   useEffect(() => {
-    if (data && d3Container.current) {
-      // Define spaces
-      const margin = { left: 120, right: 20, top: 50, bottom: 20 }
-      const width = 560 - margin.left - margin.right,
-          height = 200 - margin.top - margin.bottom
-      
-      const svg = d3.select(d3Container.current)
-          .attr('height', height + margin.top + margin.bottom)
-          .attr('width', width + margin.left + margin.right)
-      
-      // Allow space for axis
-      const chart = svg.select('.chart')
-          .attr('transform', 'translate(' + margin.left
-            + ', ' + margin.top + ')')
-  
-      // Create x and y axis
-      const x = d3.scaleLinear()
-        .domain([0, d3.max(sortedData, d => d[active])])
-        .range([0, width])
+    // Define spaces
+    const margin = { left: 120, right: 20, top: 50, bottom: 20 }
+    const width = 560 - margin.left - margin.right,
+        height = 200 - margin.top - margin.bottom
+    
+    const svg = d3.select(d3Container.current)
+        .attr('height', height + margin.top + margin.bottom)
+        .attr('width', width + margin.left + margin.right)
+    
+    // Allow space for axis
+    const chart = svg.select('.chart')
+        .attr('transform', 'translate(' + margin.left
+          + ', ' + margin.top + ')')
 
-      const xAxis = d3.axisTop(x)
-      chart.select('.x-axis')
-        .call(xAxis)
+    // Create x and y axis
+    const x = d3.scaleLinear()
+      .domain([0, d3.max(sortedData, d => d[active])])
+      .range([0, width])
+
+    const xAxis = d3.axisTop(x)
+    chart.select('.x-axis')
+      .call(xAxis)
+      .attr('font-family', 'Montserrat')
+      .attr('font-size', 14)
+
+    const y = d3.scaleBand()
+      .domain(d3.range(sortedData.length))
+      .range([height, 0])
+      .paddingInner(0.2)
+      .paddingOuter(0.3)
+
+    const yAxis = d3.axisLeft(y).tickFormat(i => sortedData[i].supplier)
+    chart.select('.y-axis')
+        .call(yAxis)
         .attr('font-family', 'Montserrat')
         .attr('font-size', 14)
 
-      const y = d3.scaleBand()
-        .domain(d3.range(sortedData.length))
-        .range([height, 0])
-        .paddingInner(0.2)
-        .paddingOuter(0.3)
-
-      const yAxis = d3.axisLeft(y).tickFormat(i => sortedData[i].supplier)
-      chart.select('.y-axis')
-          .call(yAxis)
-          .attr('font-family', 'Montserrat')
-          .attr('font-size', 14)
-  
-      // Add bars into chart
-      chart
-        .selectAll('rect')
-        .data(sortedData)
-        .join('rect')
-          .attr('x', x(0))
-          .attr('y', (d, i) => y(i))
-          .attr('width', d => x(d[active]) - x(0))
-          .attr('height', y.bandwidth())
-          .attr('fill', '#304ffe')
-    }
-
-  }, [active, data, d3Container.current]) // eslint-disable-line
+    // Add bars into chart
+    chart
+      .selectAll('rect')
+      .data(sortedData)
+      .join('rect')
+        .attr('x', x(0))
+        .attr('y', (d, i) => y(i))
+        .attr('width', d => x(d[active]) - x(0))
+        .attr('height', y.bandwidth())
+        .attr('fill', '#304ffe')
+  }, [active, data])
 
   return (
     <div>
--- src/components/top-products/TopProducts.jsx
+++ src/components/top-products/TopProducts.jsx
@@ -37,63 +37,61 @@ function TopProducts({ data }) {
   const top3Products = groupedProducts.sort((a,b) => b[active] - a[active]).slice(0,3)
 
   useEffect(() => {
-    if (data && d3Container.current) {
-      // Define spaces
-      const margin = { left: 50, right: 50, top: 50, bottom: 50 }
-      const width = 560 - margin.left - margin.right,
-          height = 200 - margin.top - margin.bottom
-      
-      const svg = d3.select(d3Container.current)
-          .attr('height', height + margin.top + margin.bottom)
-          .attr('width', width + margin.left + margin.right)
-      
-      // Allow space for axis
-      const chart = svg.select('.chart')
-          .attr('transform', 'translate(' + margin.left
-            + ', ' + margin.top + ')')
+    // Define spaces
+    const margin = { left: 50, right: 50, top: 50, bottom: 50 }
+    const width = 560 - margin.left - margin.right,
+        height = 200 - margin.top - margin.bottom
+    
+    const svg = d3.select(d3Container.current)
+        .attr('height', height + margin.top + margin.bottom)
+        .attr('width', width + margin.left + margin.right)
+    
+    // Allow space for axis
+    const chart = svg.select('.chart')
+        .attr('transform', 'translate(' + margin.left
+          + ', ' + margin.top + ')')
 
-      // Create x axis and y
-      const x = d3.scaleBand()
-        .domain(top3Products.map(d => d['productName']))
-        .range([0, width])
-        .paddingInner(0.2)
-        .paddingOuter(0.3)
+    // Create x axis and y
+    const x = d3.scaleBand()
+      .domain(top3Products.map(d => d['productName']))
+      .range([0, width])
+      .paddingInner(0.2)
+      .paddingOuter(0.3)
 
-      const xAxis = d3.axisBottom(x)
-      chart.select('.x-axis')
-        .call(xAxis)
-        .attr('transform', `translate(0, ${height})`)
-        .attr('font-family', 'Montserrat')
-        .attr('font-size', 14)
+    const xAxis = d3.axisBottom(x)
+    chart.select('.x-axis')
+      .call(xAxis)
+      .attr('transform', `translate(0, ${height})`)
+      .attr('font-family', 'Montserrat')
+      .attr('font-size', 14)
 
-      const y = d3.scaleLinear()
-        .domain([0, d3.max(top3Products, d => d[active])]).nice()
-        .range([height, 0])
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(top3Products, d => d[active])]).nice()
+      .range([height, 0])
 
-      // Add bars into chart
-      chart
-        .selectAll('rect')
-        .data(top3Products)
-        .join('rect')
-          .attr('x', d => x(d['productName']))
-          .attr('y', d => y(d[active]))
-          .attr('width', x.bandwidth())
-          .attr('height', d => y(0) - y(d[active]))
-          .attr('fill', '#304ffe')
+    // Add bars into chart
+    chart
+      .selectAll('rect')
+      .data(top3Products)
+      .join('rect')
+        .attr('x', d => x(d['productName']))
+        .attr('y', d => y(d[active]))
+        .attr('width', x.bandwidth())
+        .attr('height', d => y(0) - y(d[active]))
+        .attr('fill', '#304ffe')
 
-      // Add labels into chart
-      chart
-        .select(".labels")
-        .attr("text-anchor", "middle")
-        .attr("font-size", 12)
-        .selectAll('text')
-        .data(top3Products)
-        .join('text')
-          .text(d => d[active].toFixed(0))
-          .attr('x', d => x(d['productName']) + x.bandwidth() / 2)
-          .attr('y', d => y(d[active]) - 5)
-    }
-  }, [active, data, d3Container.current]) // eslint-disable-line
+    // Add labels into chart
+    chart
+      .select(".labels")
+      .attr("text-anchor", "middle")
+      .attr("font-size", 12)
+      .selectAll('text')
+      .data(top3Products)
+      .join('text')
+        .text(d => d[active].toFixed(0))
+        .attr('x', d => x(d['productName']) + x.bandwidth() / 2)
+        .attr('y', d => y(d[active]) - 5)
+  }, [active, data])
 
   return (
     <div>
